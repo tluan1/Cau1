2977b98c32d2cb92cb89d54e92efd28c9fe77c92
tluan1 <luanthanh253@gmail.com>
2024-02-26 09:01:37 +0700

    Add project files.

---
+++ Cau1/Cau1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, x;
+	cout << "chuong trinh tinh tong\n";
+	cout << "Nhap a:"; cin >> a;
+	cout << "Nhap b:"; cin >> b;
+	x = a + b;
+	cout << a << " +" << b << " =" << x << endl;
+	return 0;
+}
